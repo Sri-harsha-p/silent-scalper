77532461ab51264f95fa53afd6a9c0240d20bae7
Sriharsha <sriharshaup02@gmail.com>
2025-07-19 22:18:04 +0530

    env update

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ load_dotenv()
 access_key=os.getenv("ACCESS_KEY")
 secret_access_key=os.getenv("SECRET_ACCESS_KEY")
 aws_region=os.getenv("AWS_DEFAULT_REGION")
+bucket_name=os.getenv("BUCKET_NAME")
 s3 = boto3.client('s3',
                 aws_access_key_id= access_key,
                 aws_secret_access_key=secret_access_key,
                 region_name=aws_region
     )
 
+local_path="C:/Users/sriha/OneDrive/Desktop/test_1.pdf"
+key_name = os.path.basename(local_path)
 s3.upload_file(
-    Filename='D:/dental_data/images/scan42.json',
-    Bucket='silentscalper-valid',
-    Key='scan7.json'
+    Filename=local_path,
+    Bucket=bucket_name,
+    Key=key_name
 )
